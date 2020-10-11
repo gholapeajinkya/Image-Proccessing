2d3bec5566745da00fa73563a5007723ef2142fa
adityagholape <72221005+adityagholape@users.noreply.github.com>
2020-10-11 21:09:38 +0530

    Update demo.py

--- Image Processing/demo.py
+++ Image Processing/demo.py
@@ -8,8 +8,12 @@ from tkinter import messagebox
 
 
 def getImage():
-    filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-    return filename
+    try:
+        filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+        return filename
+    except: 
+        messagebox.showinfo("Alert", "Something went wrong")
+        return
 
 def saveImage():
     try:
@@ -22,39 +26,47 @@ def saveImage():
         messagebox.showinfo("Message", "first browse an image using Browse Image button")
 
 def sel(x):
-    global selection
-    selection = var.get()
-    global th
-    th = cv2.adaptiveThreshold(grayscaled, selection , cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)       
-    img2 = Image.fromarray(th)
-    img2 = img2.resize((500, 350), Image.ANTIALIAS)
-    img2 = ImageTk.PhotoImage(img2)
+    try:
+        global selection
+        selection = var.get()
+        global th
+        th = cv2.adaptiveThreshold(grayscaled, selection , cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)       
+        img2 = Image.fromarray(th)
+        img2 = img2.resize((500, 350), Image.ANTIALIAS)
+        img2 = ImageTk.PhotoImage(img2)
 
-    panel = Label(window, image = img2)
-    panel.image = img2
-    panel.place(x=515, y=10)
+        panel = Label(window, image = img2)
+        panel.image = img2
+        panel.place(x=515, y=10)
+    except: 
+        messagebox.showinfo("Alert", "Something went wrong")
+        return
 
 def proceessImage():
-    filename = getImage()
-    print(filename)
-    img = cv2.imread(filename)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(img)
-    img = img.resize((500, 350), Image.ANTIALIAS)
-    img = ImageTk.PhotoImage(img)
-    
-    panel = Label(window, image = img)
-    panel.image = img
-    panel.place(x=10, y=10)
-    global var
-    var = IntVar()
-    scale = Scale(window, variable = var ,orient=HORIZONTAL,command = sel, length=300,from_=0, to=255)
-    scale.place(x=500, y=380)
-        
-    if filename:
+    try:
+        filename = getImage()
+        print(filename)
         img = cv2.imread(filename)
-        global grayscaled
-        grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(img)
+        img = img.resize((500, 350), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+
+        panel = Label(window, image = img)
+        panel.image = img
+        panel.place(x=10, y=10)
+        global var
+        var = IntVar()
+        scale = Scale(window, variable = var ,orient=HORIZONTAL,command = sel, length=300,from_=0, to=255)
+        scale.place(x=500, y=380)
+
+        if filename:
+            img = cv2.imread(filename)
+            global grayscaled
+            grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    except: 
+        messagebox.showinfo("Alert", "Something went wrong")
+        return
         
 
 
